1d1498d8f3da74969b10771a49cd7797db0e98f7
isOdin <ivanbildagin@mail.ru>
2025-04-14 18:48:50 +0300

    change anthill

--- Anthill.h
+++ Anthill.h
@@ -1,19 +1,83 @@
-#pragma once
+#ifndef ANTHILL_H
+#define ANTHILL_H
 
-#include "Collector.h"
+//#include "Ant.h"
+#include "builder.h"
+#include "Child.h"
+#include "Cleaner.h"
+#include "Collecter.h"
+#include "Nurse.h"
+#include "Soldier.h"
+#include <iostream>
+#include <vector>
 
-using namespace std;
+#include "Informer.h"
 
-class Anthill{
+#define RoleCount 6;
+#define ENTER_COORDINATE_X 100
+#define ENTER_COORDINATE_Y 100
 
+using std::cout;
+using std::cin;
+using std::string;
+
+
+class Anthill {
 public:
-	Anthill();
-	~Anthill();
+    //getters
+    vector<Collecter*>& getCollecterList() { return collecterList; }
+    vector<Child*>& getChildList() { return childList; }
+    vector<Cleaner*>& getCleanerList() { return cleanerList; }
+    vector<Builder*>& getBuilderList() { return builderList; }
+    vector<Nurse*>& getNurseList() { return nurseList; }
+    vector<Soldier*>& getSoldierList() { return soldierList; }
 
-	vector<Collector*>& getCollectorList() {return collectors;}
+    int getEnterPosX() { return this->enterX; }
+    int getEnterPosY() { return this->enterY; }
 
-	void createAnt(const sf::Vector2f& position);
+    Anthill
+
+    (
+        int scale,
+        int x,
+        int y
+    );
+
+    void generateAnts(int posX, int posY, Informer* informer); // positions of ants in the beginning
+    void dailyResourceExpenditure();
+
+    // ежедневаня трата ресурсов муравейника, состоит из трат еды и ежедневного осыпания муравейника
 
 private:
-	vector<Collector*> collectors; // Вектор для хранения муравьев
+    //coordinates
+    int x;
+    int y;
+    int enterX; // position of anthill enter
+    int enterY;
+
+
+    // resources spending
+    void antsDailyEat();
+
+    void anthillDestroying(); // осыпь муравейника
+
+    int foodCount;
+    int scale;
+    int childrenCount;
+    int builderCount;
+    int cleanerCount;
+    int collecterCount;
+    int nurseCount;
+    int soldierCount;
+
+    int antCount;
+
+    vector<Collecter*> collecterList;
+    vector<Child*> childList;
+    vector<Builder*> builderList;
+    vector<Cleaner*> cleanerList;
+    vector<Nurse*> nurseList;
+    vector<Soldier*> soldierList;
 };
+
+#endif ANTHILL_H
